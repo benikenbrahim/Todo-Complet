54676cde5b876200ed29b73553b48e2b13650a6a
beniken brahim <benikenbrahim432@gmail.com>
2025-12-07 18:15:35 +0100

    fix home page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
+  <React.StrictMode >
+    <BrowserRouter basename="/Todo-Complet">
+      <App /> 
     </BrowserRouter>
   </React.StrictMode>
 );
