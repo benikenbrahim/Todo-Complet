7d61ce4cc2fb3f41270570a8d31959f48619c163
beniken brahim <benikenbrahim432@gmail.com>
2025-12-11 21:52:30 +0100

    2.6

--- frontend/src/pages/registrationPage.js
+++ frontend/src/pages/registrationPage.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function RegistrationPage() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
