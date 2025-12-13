fd441dfd0dab2e7ad627f76bcd117f46abb10e28
beniken brahim <benikenbrahim432@gmail.com>
2025-12-13 21:28:41 +0100

    version Deployed

--- frontend/src/routes/privet.js
+++ frontend/src/routes/privet.js
@@ -5,7 +5,7 @@ const PrivateRoute = ({ children }) => {
   const [isAuth, setIsAuth] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:5000/auth/me", {
+    fetch("https://todo-complet.onrender.com/auth/me", {
       credentials: "include" // 🔥 très important
     })
       .then(res => setIsAuth(res.ok))
